a956881f6df79ce0984d3f13791f98177bbb3d9f
K0S4 <szymonkowalski142@gmail.com>
2020-03-16 15:09:05 +0100

    Created Shop and PageHeader components

---
+++ src/components/PageHeader/PageHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './page-header.css';
+
+function PageHeader() {
+  return (
+      <header className="page-header">
+        <h1 className="page-header__title">Nearby Shops</h1>
+      </header>
+  );
+}
+
+export default PageHeader;
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./shop.css";
+
+function Shop(props) {
+  return (
+    <li className="shop open">
+      <div>
+        <p className="shop-name">{props.name}</p>
+        <p className="distance">{props.distance}</p>
+      </div>
+      <p className="closing-hour">{props.closingHour}</p>
+      <div>
+        <a>
+          <img
+            className="google-maps-icon"
+            alt="Go"
+            src="/images/google-maps-icon.png"
+          />
+        </a>
+      </div>
+    </li>
+  );
+}
+
+export default Shop;
